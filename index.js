1ea81438e0b3e74a19642560d14cab42d39221ff
tuchin1228 <1104108110@nkust.edu.tw>
2022-10-04 14:49:42 +0800

    UI更新

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const app = require('express')();
+var express = require('express');
+var app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3030;
+
+app.use(express.static(__dirname + '/style')); 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
